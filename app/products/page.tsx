77bfe4e5cddb3c570bd16d3f58945ddc358e4a3a
Vijay KC <vj.kc2007@gmail.com>
2024-01-01 23:29:01 +0200

    load more button text bug fix

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -24,7 +24,7 @@ export default async function Products({
       </div>
       <LoadMoreBtn
         buttonText={
-          currentPage === totalPages
+          allProducts.length === 100
             ? 'All products are loaded !'
             : 'Load More...'
         }
